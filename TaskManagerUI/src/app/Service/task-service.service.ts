9352f93efd89da159dd4fe68736455dc9c4c31ac
Ramachandran0403 <gnramachandran.e3@gmail.com>
2018-11-21 19:20:22 +0530

    Project Code Initial Commit

---
+++ TaskManagerUI/src/app/Service/task-service.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { Http, HttpModule, Response, Headers, RequestOptions } from '@angular/http'
+import 'rxjs/Rx'
+import { Observable } from 'rxjs'
+import { Task } from '../Models/task'
+
+@Injectable()
+export class TaskServiceService {
+  constructor(private _http: Http) {
+
+  }
+  addUrl: string = "http://localhost:50070/api/Task/AddTask";
+  updateUrl: string = "http://localhost:50070/api/Task/UpdateTask";
+  getUrl: string = "http://localhost:50070/api/Task/GetTasks";
+  getParentTaskUrl: string = "http://localhost:50070/api/Task/GetParentTasks";
+  endTaskUrl: string = "http://localhost:50070/api/Task/EndTask";
+  sharedTaskID: number;
+
+  changesharedTaskID(newTaskId: number) {
+    this.sharedTaskID = newTaskId;
+  }
+
+  getsharedTaskID() {
+    return this.sharedTaskID;
+  }
+
+  GetTask(): Observable<Task[]> {
+    return this._http.get(this.getUrl)
+      .map(response => { return response.json() })
+      .catch(error => Observable.throw(error))
+  }
+
+  GetParentTasks(taskId): Observable<string[]> {
+    return this._http.get(this.getParentTaskUrl + '/' + taskId)
+      .map(response => { return response.json() })
+      .catch(error => Observable.throw(error))
+  }
+
+  GetTaskById(taskId): Observable<Task> {
+    return this._http.get(this.getUrl + '/' + taskId)
+      .map(response => { return response.json() })
+      .catch(error => Observable.throw(error))
+  }
+
+  AddTask(task) {
+    let body = JSON.stringify(task);
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.post(this.addUrl, task, options)
+      .catch(error => Observable.throw(error));
+  }
+
+  UpdateTask(task) {
+    let body = JSON.stringify(task);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.post(this.updateUrl, body, options)
+      .catch(error => Observable.throw(error));
+  }
+
+  EndTask(taskId) {
+    let body = JSON.stringify(taskId);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.post(this.endTaskUrl, body, options)
+      .map(response => { return response.json() })
+      .catch(error => Observable.throw(error));
+  }
+}
